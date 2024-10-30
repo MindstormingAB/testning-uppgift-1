5221da4cf9c94897507b38ef0bad9f732dc79015
MindstormingAB <s.elander@mindstorming.se>
2024-10-30 23:01:41 +0100

    Added user class and user data

---
+++ modules/userData.js
@@ -0,0 +1,11 @@
+import User from './User';
+
+const users = [
+  new User('Alice Adamsson', 'kYCpASSBAO8Zn2G'),
+  new User('Bruno Brynolfsson', '7W2fLI4RZvxjiZn'),
+  new User('Calle Carlsson', '16j4fEGxmnFmlCz'),
+  new User('David Didriksson', 'jqrYRL5i87yoiAJ'),
+  new User('Eva Elofsson', 'lS6q5gO9ULprHBN'),
+];
+
+export default users;
